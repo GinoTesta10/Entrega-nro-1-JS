6f237a768c23db8b181e5e90b3c4792a8cf2daad
ginotesta <testagino10@gmail.com>
2025-07-22 16:31:20 -0300

    correccion

--- main.js
+++ main.js
@@ -1,42 +1,17 @@
 function promedionotas() {
   let alumno = prompt("ingresa tu nombre");
   const notas = parseFloat(prompt("Cuantas notas deseas promediar?"));
-  let cantidadtotal = 0;
-  let notastotales = 0;
-
-  if (notas > 0 || isNaN(notas)) {
-    notastotales = notastotales + notas;
-  } else {
-    alert("ingresa numeros para poder continuar");
-    return;
-  }
-
-  for (let i = 1; i <= notas; i++) {
-    const evaluaciones = parseFloat(prompt("Ingrese las notas"));
-    if (evaluaciones > 0) {
-      cantidadtotal = cantidadtotal + evaluaciones;
-    } else {
-      alert("Por favor, ingrese numeros validos");
-      return;
+  let suma = 0;
+  {
+    if (notas > 0) {
+      for (let i = 1; i <= notas; i++) {
+        let notatotal = parseInt(prompt("ingrese sus notas"));
+        suma += notatotal;
+      }
     }
   }
-  alert(alumno + " su promedio es de:" + dividir(cantidadtotal, notastotales));
+  const promedio = suma / notas;
+  console.log(alumno + "tu promedio es " + promedio);
 }
+
 promedionotas();
-
-function sumadenotas(a, b) {
-  a + b;
-}
-
-function dividir(a, b) {
-  a = parseFloat(a);
-  b = parseFloat(b);
-  if (isNaN(a) || isNaN(b)) {
-    return "Error: Valores no numéricos";
-  }
-  if (b === 0) {
-    return "Error: División por cero";
-  }
-
-  return a / b;
-}
